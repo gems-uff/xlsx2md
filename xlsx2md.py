694e0ff0194dd7cb9c695665306e65fbd002636f
Leonardo Gresta Paulino Murta <leomurta@ic.uff.br>
2023-04-02 15:35:52 -0300

    Adds all files

---
+++ xlsx2md.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import sys
+
+file = sys.argv[1]
+df = pd.read_excel(file, "Cronograma", keep_default_na=False)
+
+print('{: style="border-collapse: collapse; width: 100%; text-align: center;" border="1"}')
+print('| Data | Atividade | Entrega')
+print('|---')
+
+for index, row in df.iterrows():
+    date = pd.to_datetime(row['Data'], errors='coerce').strftime('%d/%m/%Y')
+    activity = row['Atividade']
+
+    if activity:
+        if activity.startswith(('Prova', 'Segunda chamada', 'Verificação suplementar')):
+            color = 'blue'
+        elif activity.startswith('Apresentação de trabalho'):
+            color = 'green'
+        elif activity.startswith(('Sem aula', 'Vista de prova')):
+            color = 'red'
+        else:
+            color = None
+
+        if color:
+            print(f'| {date} | <span style="color:{color}">{activity}</span>')
+        else:
+            print(f'| {date} | Aula')
